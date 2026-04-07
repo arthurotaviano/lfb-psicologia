2998b4df9f69bd69688102b0b4a2c1f612b1f81b
Arthur Otaviano <oarthurotaviano@gmail.com>
2026-04-07 00:06:36 -0300

    style: add footer border

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import FooterLinks from './FooterLinks'
 
 export default function Footer() {
   return (
-    <footer className='py-5 md:py-10 text-sm'>
+    <footer className='border-t border-taupe-300 py-5 md:py-10 text-sm'>
       <Container className='flex flex-col md:flex-row md:justify-between md:items-center gap-2 md:gap-10'>
         <div>LFB Consultório de Psicologia - {new Date().getFullYear()}</div>
         <FooterLinks />
