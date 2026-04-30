011c70a32382c2b3189f1ee13672f41b73f316b6
Arthur Otaviano <oarthurotaviano@gmail.com>
2026-04-30 14:17:22 -0300

    feat: add homepage location section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { HomeAbout } from '@/components/home/about'
+import { HomeLocation } from '@/components/home/location'
 
 export default function Home() {
   return (
     <>
       <HomeAbout />
+      <HomeLocation />
     </>
   )
 }
