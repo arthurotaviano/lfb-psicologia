1b0c36f0b7ac758b33eff06def335a687defadb3
Arthur Otaviano <oarthurotaviano@gmail.com>
2026-04-30 15:24:41 -0300

    fix: replace a tag for next Link in blog list

--- src/components/blog-list.tsx
+++ src/components/blog-list.tsx
@@ -1,9 +1,11 @@
+import Link from 'next/link'
+
 export function BlogList() {
   return (
     <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8'>
       {Array.from({ length: 6 }).map((_, i) => (
         <li key={i}>
-          <a className='hover:scale-105 transition-transform duration-300 flex flex-col gap-2 rounded-2xl border border-taupe-300 px-6 py-4 bg-white' href='/blog/titulo-do-post' aria-label='Título do post'>
+          <Link className='hover:scale-105 transition-transform duration-300 flex flex-col gap-2 rounded-2xl border border-taupe-300 px-6 py-4 bg-white' href='/blog/titulo-do-post' aria-label='Título do post'>
             <span className='text-xs' aria-hidden='true'>
               01/01/2026
             </span>
@@ -12,7 +14,7 @@ export function BlogList() {
               <span className='rounded-full border border-taupe-300 w-5 h-5 bg-accent/5'></span>
               <span>Nome da Autora</span>
             </span>
-          </a>
+          </Link>
         </li>
       ))}
     </ul>
